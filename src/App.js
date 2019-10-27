92d1e6ebc021fed8267b5b8d24c47bbcaf611faf
Jonah Fidel <jonahfidel@gmail.com>
2019-10-27 14:31:21 -0400

    Added task form.

--- src/App.js
+++ src/App.js
@@ -28,6 +28,36 @@ export default class App extends PureComponent {
             <td>This one will take a while.</td>
           </tr>
         </table>
+        <br></br>
+        <div class="container">
+        <form>
+          <div class="form-group">
+            <label for="nameOfTask">Task Name</label>
+            <input type="text" class="form-control" id="nameOfTask" placeholder="Enter a task"></input>
+          </div>
+          <div class="form-group">
+            <label for="taskDueDate">Due Date</label>
+            <input type="date" class="form-control" id="taskDueDate"></input>
+          </div>
+          <div class="form-group">
+            <label for="taskType">Type of Task</label>
+            <input type="text" class="form-control" id="taskType" placeholder="project"></input>
+          </div>
+          <div class="form-group">
+            <label for="isFinished">isFinished</label>
+            <input type="boolean" class="form-control" id="isFinished"></input>
+          </div>
+          <div class="form-group">
+            <label for="notes">Notes</label>
+            <textarea type="textarea" class="form-control" id="notes"></textarea>
+          </div>
+          {/* <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div> */}
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </div>
       </div>
     );
   }
