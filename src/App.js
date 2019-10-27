1807005fc7b2ca8fe818809e7a366d07d1af2795
Jonah Fidel <jonahfidel@gmail.com>
2019-10-27 14:05:41 -0400

    Initial commit to forked repo.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,23 @@ export default class App extends PureComponent {
     return (
       <div>
         <Header />
+        <table>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Age</th>
+          </tr>
+          <tr>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>Eve</td>
+            <td>Jackson</td>
+            <td>94</td>
+          </tr>
+        </table>
       </div>
     );
   }
