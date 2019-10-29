75070fd25b69c6820fb62acbcdd2d69f0dfd54d1
Jonah Fidel <jonahfidel@gmail.com>
2019-10-29 16:40:03 -0400

    Connect to local database.

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,39 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const sqlite3 = require('sqlite3').verbose();
+let myNewVar;
+let db = new sqlite3.Database('./db/chinook.db', (err)=>{
+  if (err){
+    console.log(err.message);
+  }
+
+  console.log("connected to chinook db");
+  myNewVar = "connected to chinook db wawaawasdf";
+});
+
+db.serialize(() => {
+    db.each(`SELECT PlaylistId as id,
+                    Name as name
+             FROM playlists`, (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log(row.id + "\t" + row.name);
+      myNewVar += row.id + "\t" + row.name;
+    });
+  });
+
+db.close((err) => {
+    if (err){
+      console.log(err.message);
+    }
+
+    console.log("closed chinook db connection");
+});
 
 router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+    res.send(myNewVar);
 });
 
 module.exports = router;
