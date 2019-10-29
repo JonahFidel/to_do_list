4166808c1770f1ad9f8450e636cad28fa3bec5d0
Jonah Fidel <jonahfidel@gmail.com>
2019-10-29 12:55:13 -0400

    Add and remove tasks working.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,48 @@ import ReactDOM from "react-dom";
 import Header from "./Header";
 
 export default class App extends PureComponent {
-  handler(task) {
-    task.preventDefault(); // don't refresh the page upon form submit
+  
+  // probably should rename this
+  onClick(task) {
     const taskList = this.state.taskList;
-    let firstTask = {
-      name: "firstTask",
-      date: "12/8/1992",
-      type: "project",
-      isFinished: "false",
-      notes: "Tgreen eggs and ham."
+    console.log(task);
+    // might want to arrange this as a dictionary
+    let newTask = {
+      name: task[0],
+      date: task[1],
+      type: task[2],
+      isFinished: task[3],
+      notes: task[4]
     }
     this.setState({
-      taskList: taskList.concat(firstTask),
+      taskList: taskList.concat(newTask),
+      taskListLength: taskList.length,
+    })
+  }
+
+  removeItem(i){
+    let taskList = this.state.taskList;
+
+    // necessary because splice alters the array in place and we need to keep it immutable bc react
+    const newTaskListStart = taskList.slice(0, i);
+    const newTaskListEnd = taskList.slice(i + 1, this.state.taskListLength);
+    
+    taskList = newTaskListStart.concat(newTaskListEnd);
+    this.setState({
+      taskList: taskList, 
+      taskListLength: taskList.length,
     })
   }
 
   constructor(props) {
     super(props);
-    this.handler = this.handler.bind(this);
+    this.removeItem = this.removeItem.bind(this);
     let firstTask = {
       name: "firstTask",
       date: "12/8/1992",
       type: "project",
       isFinished: "false",
-      notes: "Tgreen eggs and ham."
+      notes: "green eggs and ham."
     }
     let secondTask = {
       name: "secondTask",
@@ -49,6 +67,7 @@ export default class App extends PureComponent {
           thirdTask
         // "shopping", "cleaning", "house work",
       ],
+      taskListLength: 3,
     };
   }
 
@@ -56,8 +75,8 @@ export default class App extends PureComponent {
     return (
       <div>
         <Header />
-        <TaskList value={this.state} />
-        <TaskForm handler={this.handler}/>
+        <TaskList action={this.removeItem} value={this.state} />
+        <TaskForm onClick={this.onClick.bind(this)}/>
       </div>
     );
   }
@@ -66,16 +85,14 @@ export default class App extends PureComponent {
 class Task extends React.Component {
   render() {
     return (
-      <tbody>
-      <tr>
+        <tr>
             <td>{this.props.name}</td>
             <td>{this.props.date}</td>
             <td>{this.props.type}</td>
             <td>{this.props.isFinished}</td>
             <td>{this.props.notes}</td>
+            <td><button onClick={() => this.props.action(this.props.id)}>Remove</button></td>
           </tr>
-      </tbody>
-     
     );
   }
 }
@@ -87,10 +104,11 @@ class TaskList extends React.Component {
     let taskList = this.props.value.taskList;
 
     for (let i = 0; i < taskList.length; i++) {
-      items.push(<Task key={i} name={taskList[i].name} date={taskList[i].date} type={taskList[i].type} isFinished={taskList[i].isFinished} notes={taskList[i].notes}/>)
+      items.push(<Task key={i} id={i} action={this.props.action} name={taskList[i].name} date={taskList[i].date} type={taskList[i].type} isFinished={taskList[i].isFinished} notes={taskList[i].notes}/>)
     }
     return (
       <table className="table">
+        <tbody>
         <tr>
           <th>Name</th>
           <th>Due Date</th>
@@ -99,12 +117,15 @@ class TaskList extends React.Component {
           <th>Notes</th>
         </tr>
         {items}
+        </tbody>
       </table>
     );
   }
 }
 
 class TaskForm extends React.Component {
+  
+  
   render() {
 
 
@@ -135,9 +156,22 @@ class TaskForm extends React.Component {
             <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
             <label class="form-check-label" for="exampleCheck1">Check me out</label>
           </div> */}
-          <button className="btn btn-primary" onClick={this.props.handler}>Submit</button>
+          <button className="btn btn-primary" onClick={this.update.bind(this)}>Submit</button>
         </form>
       </div>
     );
+  }
+
+  update (e) {
+    e.preventDefault(); // don't refresh the page upon form submit
+    let newTask = 
+      [
+        this.refs.nameOfTask.value,
+        this.refs.taskDueDate.value,
+        this.refs.taskType.value,
+        this.refs.isFinished.value,
+        this.refs.notes.value
+      ];
+    this.props.onClick(newTask);
   }
 }
